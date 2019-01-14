5d9c7760c09820b81dc933ab336e26feca8d0cd0
bouchja1 <jan.bouchner@gmail.com>
2019-01-14 17:42:48 +0100

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+// https://pm2.io/doc/en/runtime/guide/easy-deploy-with-ssh
+module.exports = {
+    apps: [{
+        name: "kindergarten-api",
+        script: "app/index.js"
+    }],
+    deploy: {
+        // "production" is the environment name
+        production: {
+            // SSH key path, default to $HOME/.ssh
+            key: "~/.ssh/pm2_deploy_key",
+            // SSH user
+            user: "travis",
+            // SSH host
+            host: ["185.8.165.64"],
+            // SSH options with no command-line flag, see 'man ssh'
+            // can be either a single string or an array of strings
+            ssh_options: "StrictHostKeyChecking=no",
+            // GIT remote/branch
+            ref: "origin/master",
+            // GIT remote
+            repo: "git@github.com:bouchja1/kindergarten-api.git",
+            // path in the server
+            path: "/home/travis/projects/kindergarten-api",
+            // Pre-setup command or path to a script on your local machine
+            "pre-setup": "ls -la",
+            // Post-setup commands or path to a script on the host machine
+            // eg: placing configurations in the shared dir etc
+            "post-setup": "ls -la",
+            // pre-deploy action
+            "pre-deploy":"git pull",
+            "pre-deploy-local": "echo 'This is a local executed command'",
+            // post-deploy action
+            "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+        },
+    }
+}
