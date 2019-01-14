bf377fbed1931ad199f07501b1c6235acac5021d
bouchja1 <jan.bouchner@gmail.com>
2019-01-14 19:03:46 +0100

    travis

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,38 @@ module.exports = {
     }],
     deploy: {
         // "production" is the environment name
-        production: {
+        /*
+        productionLocal: {
             // SSH key path, default to $HOME/.ssh
             key: "~/.ssh/travis_pm2_rsa",
+            // SSH user
+            user: "travis",
+            // SSH host
+            host: ["185.8.165.64"],
+            // SSH options with no command-line flag, see 'man ssh'
+            // can be either a single string or an array of strings
+            ssh_options: "StrictHostKeyChecking=no",
+            // GIT remote/branch
+            ref: "origin/master",
+            // GIT remote
+            repo: "git@github.com:bouchja1/kindergarten-api.git",
+            // path in the server
+            path: "/home/travis/projects/kindergarten-api",
+            // Pre-setup command or path to a script on your local machine
+            "pre-setup": "ls -la",
+            // Post-setup commands or path to a script on the host machine
+            // eg: placing configurations in the shared dir etc
+            "post-setup": "ls -la",
+            // pre-deploy action
+            "pre-deploy":"git pull",
+            "pre-deploy-local": "echo 'This is a local executed command'",
+            // post-deploy action
+            "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+        },
+        */
+        production: {
+            // SSH key path, default to $HOME/.ssh
+            key: "/home/travis/.ssh/travis_pm2_rsa",
             // SSH user
             user: "travis",
             // SSH host
